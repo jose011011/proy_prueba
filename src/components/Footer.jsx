18da80d81713988f7eb1242838501ff52467c354
José Mamani <juanmamani121432@gmail.com>
2025-06-16 16:09:03 -0400

    Eliminado Footer.css y migrado estilos a Bootstrap

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import "./Footer.css";
 
 const Footer = () => (
-  <footer className="footer">
-    <div className="footer-container">
-      <div className="footer-left">
-        <p>
-          © 2025 derechos reservados <hr /> Juan José mamani machaca
-          <br />
+  <footer className="bg-dark text-white py-4 text-center">
+    <div className="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+      <div className="mb-3 mb-md-0">
+        <p className="mb-0">
+          © 2025 derechos reservados <br /> Juan José Mamani Machaca
         </p>
       </div>
-      <div className="footer-right">
-        <h3>Publicidad de la materia</h3>
-        <button className="btn-publicidad">WEB 2</button>
+      <div>
+        <h5 className="mb-2">Publicidad de la materia</h5>
+        <button className="btn btn-success">WEB 2</button>
       </div>
     </div>
   </footer>
